d1f30d89b6277dd3afcb6bd9fca3ea2c5d902903
Ujjawal-0103 <ujjawalagrawal01032007@gmail.com>
2026-05-23 20:16:33 +0530

    2nd chsnge

---
+++ Day_1/first.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class first{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+
+        System.out.print("The sum upto " + n + "th " + "natural number is: ");
+        System.out.print(sum);
+    }
+}
